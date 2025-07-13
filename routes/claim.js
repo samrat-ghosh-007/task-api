047e8b53813bd4582ac3f0581f4dc05afda0c902
samrat-ghosh-007 <samrat9ghosh2018@gmail.com>
2025-07-13 17:25:13 +0530

    Fixed file paths(2)

--- routes/claim.js
+++ routes/claim.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User');
+const User = require('../models/users');
 const History = require('../models/history');
 
 // POST claim points
