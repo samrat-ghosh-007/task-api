be4dc494ac528e0c194f417aab17d8b21c1a15b6
samrat-ghosh-007 <samrat9ghosh2018@gmail.com>
2025-07-14 16:49:35 +0530

    Fixed cors issues(3)

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 
 
 app.use(cors({
-  origin: ['https://task-frontend-azure.vercel.app/'], 
+  origin: ['https://task-frontend-azure.vercel.app'], 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: false
 }));
