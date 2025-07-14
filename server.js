ddebc7d2c0f1a453f6f9e97be37c95684875ccdc
samrat-ghosh-007 <samrat9ghosh2018@gmail.com>
2025-07-14 16:53:35 +0530

    Fixed cors issues(4)

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ app.use(cors({
   credentials: false
 }));
 
+app.options('*', cors());
+
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
